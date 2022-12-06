c7ddde1c55545ffa2f0ea55185d9a2e4ee079780
Alhajideen <salifunurudeen834@gmail.com>
2022-12-06 09:40:59 -0600

    Insert second data in animals table

--- schema.sql
+++ schema.sql
@@ -8,8 +8,3 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL NOT NULL
 );
-
-INSERT INTO animals(id,name,
-date_of_birth,escape_attempts,
-neutered,weight_kg) VALUES (1,
-'Agumon', '2020-02-03',0,true,10.23)
