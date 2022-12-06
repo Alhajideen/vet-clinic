b456dfe9de6cad2bfc6defbe7392bc7d17e8aa78
Alhajideen <salifunurudeen834@gmail.com>
2022-12-06 09:36:51 -0600

    insert a data in animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL NOT NULL
 );
+
+INSERT INTO animals(id,name,
+date_of_birth,escape_attempts,
+neutered,weight_kg) VALUES (1,
+'Agumon', '2020-02-03',0,true,10.23)
