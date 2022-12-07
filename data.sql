f7d5f83a9efc95d75083f694f6de48644d266c4b
Alhajideen <salifunurudeen834@gmail.com>
2022-12-07 06:06:02 -0600

    add multiple columns to the animals table

--- data.sql
+++ data.sql
@@ -21,3 +21,21 @@ INSERT INTO animals(id,name,
 date_of_birth,escape_attempts,
 neutered,weight_kg) VALUES (4,
 'Devimon', '2017-05-12',5,true,11);
+
+-- DAY 2
+
+INSERT INTO animals(id,name,
+date_of_birth,escape_attempts,
+neutered,weight_kg) VALUES (5,
+'Charmander', '2020-02-08',0,false,11);
+
+-- ADDING MULTIPLE COLUMNS AT ONCE
+INSERT INTO animals (id,name,
+date_of_birth,weight_kg,neutered,escape_attempts)
+VALUES 
+(6,'Plantmon','2021-11-15',5.7,true,2),
+(7,'Squirtle','1993-04-02',12.13,false,3),
+(8,'Angemon','2005-06-12',45,true,1),
+(9,'Boarmon','2005-06-07',20.4,true,7),
+(10,'Blossom','1998-10-13',17,true,3),
+(11,'Ditto','2002-05-14',22,true,4);
