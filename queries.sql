86862ab0474bac9c1e3c605ccb14329803066f36
Alhajideen <salifunurudeen834@gmail.com>
2022-12-07 06:40:16 -0600

    delete all records from animals and rollback

--- queries.sql
+++ queries.sql
@@ -8,3 +8,33 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- UPDATE SPECIES;
+BEGIN;
+
+UPDATE animals 
+SET species= 'unspecified';
+
+-- check if update was successful
+SELECT species from animas;
+
+ROLLBACK;
+
+-- A NEW TRANSACTION
+BEGIN;
+
+UPDATE animals
+SET species='digimon' 
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species='' 
+WHERE species=digimon;
+
+-- Delete all records from animals
+BEGIN;
+
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
